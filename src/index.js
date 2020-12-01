c8dca12a03ca6a53798e90c79b3e28592c20bebb
Max Pinheiro <maxpinheiro181@gmail.com>
2020-12-01 12:26:26 -0500

    basic pages & routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 import './index.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import reportWebVitals from './reportWebVitals';
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Details from "./pages/Details";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path={["/profile", "/profile/{profileId}"]} component={Profile} />
+        <Route path="/search" component={Search} />
+        <Route path="/details/{detailsId}" component={Details} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Details extends React.Component {
+    render() {
+        return (
+            <div>
+                Details Page
+            </div>
+        );
+    }
+}
+
+export default Details;
